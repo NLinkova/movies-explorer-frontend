6934a1b0e315d3d86eeefbb9626b87d82b47ea2b
Natalia Linkova <linkova1109@yandex.ru>
2022-06-23 10:42:16 +1000

    tech

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,15 +3,15 @@
 import AboutProject from "../AboutProject/AboutProject";
 // import Portfolio from "./Portfolio/Portfolio";
 import Promo from "../Promo/Promo";
-// import Techs from "./Techs/Techs";
+import Techs from "../Techs/Techs";
 
 function Main() {
   return (
     <div className="Main">
       <Promo />
       <AboutProject />
-      {/* <Techs />
-      <AboutMe/>
+     <Techs />
+       {/* <AboutMe/>
       <Portfolio /> */}
     </div>
   );
