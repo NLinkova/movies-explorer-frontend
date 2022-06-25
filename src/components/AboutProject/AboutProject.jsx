41c9ce1b872706a32c7cb8b01d390b1a3fc31c80
Natalia Linkova <linkova1109@yandex.ru>
2022-06-25 11:23:59 +1000

    main mobile view

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -8,33 +8,33 @@ function AboutProject() {
         О проекте
       </h2>
       <ul className="about__description">
-        <li className="about__description_item">
-          <h3 className="about__description_title">
+        <li className="about__description-item">
+          <h3 className="about__description-title">
             Дипломный проект включал 5 этапов
           </h3>
-          <p className="about__description_content">
+          <p className="about__description-content">
             Составление плана, работу над бэкендом, вёрстку, добавление
             функциональности и финальные доработки.
           </p>
         </li>
-        <li className="about__description_item">
-          <h3 className="about__description_title">
+        <li className="about__description-item">
+          <h3 className="about__description-title">
             На выполнение диплома ушло 5 недель
           </h3>
-          <p className="about__description_content">
+          <p className="about__description-content about__description-content_last">
             У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
             соблюдать, чтобы успешно защититься.
           </p>
         </li>
       </ul>
       <ul className="about__duration">
-        <li className="about__duration_item">
+        <li className="about__duration-item">
           <p className="about__duration-title about__duration-title_backend">
             1 неделя
           </p>
           <p className="about__duration-subtitle">Back-end</p>
         </li>
-        <li className="about__duration_item">
+        <li className="about__duration-item">
           <p className="about__duration-title">4 недели</p>
           <p className="about__duration-subtitle">Front-end</p>
         </li>
