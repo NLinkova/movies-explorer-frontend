5f4ffed85c5d5c3b2ea3f6aae21bd68dabb487d2
Natalia Linkova <linkova1109@yandex.ru>
2022-06-29 11:02:47 +1000

    profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,12 @@ import Preloader from '../Preloader/Preloader';
 import './Profile.css';
 
 function Profile({ signOut, updateUser, isLoading, textError }) {
-  const currentUser = useContext(CurrentUserContext);
+  // const currentUser = useContext(CurrentUserContext);
+  //временное решение для разработки интерфейса
+  const currentUser = {
+    'name': 'Наталья',
+    'email': 'email11@email.com'
+  };
   const form = useForm({ name: currentUser.name, email: currentUser.email });
 
   function handleSubmit(e) {
@@ -17,70 +22,72 @@ function Profile({ signOut, updateUser, isLoading, textError }) {
     });
   }
   console.log(textError);
-  return (
-    <section className="profile">
-      <div className="profile__container">
-        <h2 className="profile__title">{`Привет, ${currentUser.name}!`}</h2>
-        <form className="profile__form" onSubmit={handleSubmit}>
-          <label className="profile__field">
-            <span className="profile__name">Имя</span>
-            <input
-              className="profile__input"
-              name="name"
-              type="text"
-              onChange={form.handleChange}
-              value={form.values.name || ''}
-              minLength="2"
-              maxLength="40"
-              required
-              placeholder="Имя"
-            />
-          </label>
-          <div className="profile__error">{`${
-            form.errors.name ? form.errors.name : ''
-          }`}</div>
-          <div className="profile__line"></div>
-          <label className="profile__field">
-            <span className="profile__name">Почта</span>
-            <input
-              className="profile__input"
-              name="email"
-              type="email"
-              onChange={form.handleChange}
-              value={form.values.email || ''}
-              minLength="2"
-              maxLength="40"
-              required
-              placeholder="Почта"
-            />
-          </label>
-          <div className="profile__error">{`${
-            form.errors.email ? form.errors.email : ''
-          }`}</div>
-          <div className="profile__preloader">{isLoading && <Preloader />}</div>
-          <span
-            className={`profile__text-error ${
-              textError && 'profile__text-error_visible'
-            }`}
-          >
-            {textError && textError}
-          </span>
-          <button
-            type="submit"
-            className={`profile__btn-edit ${
-              !form.isValid && 'profile__btn-edit_disabled'
-            }`}
-            disabled={!form.isValid}
-          >
-            Редактировать
+
+
+  if (isLoading) {
+    return <Preloader />
+  } else {
+    return (
+      <section className="profile">
+        <div className="profile__container">
+          <h1 className="profile__title">{`Привет, ${currentUser.name}!`}</h1>
+          <form className="profile__form" onSubmit={handleSubmit}>
+            <label className="profile__field">
+              <span className="profile__name">Имя</span>
+              <input
+                className="profile__input"
+                name="name"
+                type="text"
+                onChange={form.handleChange}
+                value={form.values.name || ''}
+                minLength="2"
+                maxLength="40"
+                required
+              />
+            </label>
+            <span className="profile__error">{`${
+              form.errors.name ? form.errors.name : ''
+            }`}</span>
+            <label className="profile__field">
+              <span className="profile__name">E-mail</span>
+              <input
+                className="profile__input"
+                name="email"
+                type="email"
+                onChange={form.handleChange}
+                value={form.values.email || ''}
+                minLength="2"
+                maxLength="40"
+                required
+              />
+            </label>
+            <span className="profile__error">{`${
+              form.errors.email ? form.errors.email : ''
+            }`}</span>
+            {/* <span
+              className={`profile__text-error ${
+                textError && 'profile__text-error_visible'
+              }`}
+            >
+              {textError && textError}
+            </span> */}
+            <button
+              type="submit"
+              className={`profile__btn ${
+                !form.isValid && 'profile__btn_disabled'
+              }`}
+              disabled={!form.isValid}
+            >
+              Редактировать
+            </button>
+          </form>
+          <button type="button" className="profile__out" onClick={signOut}>
+            Выйти из аккаунта
           </button>
-        </form>
-        <button type="button" className="profile__out" onClick={signOut}>
-          Выйти из аккаунта
-        </button>
-      </div>
-    </section>
-  );
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Profile;
